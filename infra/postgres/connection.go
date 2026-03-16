262890aa88a4c50419ed4cf3f9d55245ba176ac9
ggualbertosouza <contatoguilhermegsz@gmail.com>
2026-03-16 15:38:03 -0300

    add: gorm function to return gorm conn

--- infra/postgres/connection.go
+++ infra/postgres/connection.go
@@ -1,12 +1,44 @@
 package postgres
 
-import "database/sql"
+import (
+	"database/sql"
+
+	gormDriver "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 var db *sql.DB
+var gormDB *gorm.DB
 
 func DB() *sql.DB {
 	if db == nil {
 		panic("postgres not initialized")
 	}
 	return db
+}
+
+func DBGorm() *gorm.DB {
+
+	if db == nil {
+		panic("postgres not initialized")
+	}
+
+	if gormDB != nil {
+		return gormDB
+	}
+
+	gdb, err := gorm.Open(
+		gormDriver.New(gormDriver.Config{
+			Conn: db,
+		}),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	gormDB = gdb
+
+	return gormDB
 }
